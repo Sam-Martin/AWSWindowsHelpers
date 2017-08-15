520eb9f4e86511f2e105fa50cefd266dd4667003
Sam Martin <smartin@sdl.com>
2017-08-15 10:36:46 +0100

    Removed unused test and build steps from psake

--- psake.ps1
+++ psake.ps1
@@ -25,43 +25,7 @@ Task Init {
     "`n"
 }
 
-Task Test -Depends Init  {
-    '----------------------------------------------------------------------'
-    
-    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
-    $PSVersion = $PSVersionTable.PSVersion.Major
-    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
-        
-    "`n`tSTATUS: Testing with PowerShell $PSVersion"
-
-    # Gather test results. Store them in a variable and file
-    $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -ExcludeTag Integration
-
-    # In Appveyor?  Upload our tests! #Abstract this into a function?
-    If($ENV:BHBuildSystem -eq 'AppVeyor')
-    {
-        (New-Object 'System.Net.WebClient').UploadFile(
-            "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
-            "$ProjectRoot\$TestFile" )
-    }
-
-    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
-
-    # Failed tests?
-    # Need to tell psake or it will proceed to the deployment. Danger!
-    if($TestResults.FailedCount -gt 0)
-    {
-        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
-    }
-    "`n"
-}
-
-Task Build -Depends Test {
-    '----------------------------------------------------------------------'
-    #Set-ModuleFunctions
-}
-
-Task Deploy -Depends Build {
+Task Deploy -Depends Init {
     '----------------------------------------------------------------------'    
     # Update Manifest version number
     $ManifestPath = $Env:BHPSModuleManifest
