43753ddc702564bd8be523f11271739c4f5c1219
Sam Martin <smartin@sdl.com>
2017-10-24 09:52:14 +0100

    Added Get-AWSWindowsHelperAuroraStorage

---
+++ AWSWindowsHelpers/public/Get-AWSWindowsHelperAuroraStorage.ps1
@@ -0,0 +1,43 @@
+﻿function Get-AWSWindowsHelperALBTraffic{
+    param(
+        [parameter(Mandatory=$True)]
+        [string]$AWSRegion,
+        [parameter(Mandatory=$True)]
+        [string]$RDSClusterName,
+        [parameter(Mandatory=$True)]
+        [datetime]$StartTime,
+        [parameter(Mandatory=$True)]
+        [datetime]$EndTime,
+        [int]$Period = $(24 * 60 * 60),
+        [switch]$MaxInRange
+    )
+    Write-Verbose "Getting VolumeBytesUsed for $RDSClusterName"
+    $Params = @{
+        Namespace = 'AWS/RDS' 
+        MetricName = 'VolumeBytesUsed' 
+        Dimension = @(
+            @{"Name"="DbClusterIdentifier";Value=$RDSClusterName} 
+            @{"Name"="EngineName";Value="aurora"} 
+        )
+        Region = $AWSRegion
+        StartTime = $StartTime
+        EndTime = $EndTime
+        Period = $Period
+        Statistic = 'Maximum'
+    }
+    $MetricStatistics = Get-CWMetricStatistic @Params
+
+    if($MaxInRange){
+        New-Object psobject -Property @{
+            "MegabytesUsed" = ($MetricStatistics.Datapoints.Maximum | Measure-Object -Maximum).Maximum/1MB
+            "Customer" = $Customer
+            "ClusterName" = $RDSCluster
+        }
+    }else{
+        New-Object psobject -Property @{
+            "BytesUsed" = $MetricStatistics
+            "Customer" = $Customer
+            "ClusterName" = $RDSCluster
+        }
+    }
+}
