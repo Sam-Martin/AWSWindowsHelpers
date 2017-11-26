58531ab02cf60493d1b1b38ce833be24242f1d4f
Oliver Li <oliver.1i@outlook.com>
2017-11-26 15:20:09 +0000

    Remove New CFN Parameter function
    Update ConvertTo CFN Parameter function to remove dependancy on New CFN parameter function and add parameter for usePreviousValue

--- AWSWindowsHelpers/public/CloudFormation/ConvertTo-AWSWindowsHelperCFNParameter.ps1
+++ AWSWindowsHelpers/public/CloudFormation/ConvertTo-AWSWindowsHelperCFNParameter.ps1
@@ -3,13 +3,18 @@ function ConvertTo-AWSWindowsHelperCFNParameter
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
-        [hashtable]$cfnParamHashTable
+        [hashtable]$cfnParamHashTable,
+        [boolean]$usePreviousValue=$true
     )
     $cfnParameterArray = @()
 
     foreach($item in $cfnParamHashTable.GetEnumerator())
     {
-        $cfnParameterArray += New-AWSWindowsHelperCFNParameter -key $item.Name -value $item.Value
+        $cfnParameterArray += @{
+            Key = $item.Name
+            Value = $item.Value
+            UsePreviousValue = $usePreviousValue
+        }
     }
    Return $cfnParameterArray
 }
